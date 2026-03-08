e2081b966aecda56a4757d778daae7066a49ece6
am5ari <amari.taylor0305@gmail.com>
2026-03-07 23:35:30 -0500

    full function finished

--- insertion.py
+++ insertion.py
@@ -5,3 +5,8 @@ def monotonic_insertion(arr):
         key = arr[i]
         j = i - 1
 
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
